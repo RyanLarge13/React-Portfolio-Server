a1ca50f0f72fa115aebad8c09cc9e3fd2c4a847c
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2022-12-10 19:28:30 -0800

    Completing the routes to serve personal blog posts.

--- controllers/devController.js
+++ controllers/devController.js
@@ -10,7 +10,22 @@ const getProjectsFromCollection = async () => {
   }
 };
 
+const getBlogsFromCollection = async () => {
+  try {
+    let blogCollection = mongoose.connection.collection("blogs");
+    let blogs = await blogCollection.find({}).toArray();
+    return blogs;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 export const sendProjects = async (req, res) => {
   const projects = await getProjectsFromCollection();
   res.status(200).json({ data: projects });
+};
+
+export const sendBlogs = async (req, res) => {
+  const blogs = await getBlogsFromCollection();
+  res.status(200).json({ blogData: blogs });
 };
--- routes/devRouter.js
+++ routes/devRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { sendProjects } from "../controllers/devController.js";
+import { sendProjects, sendBlogs } from "../controllers/devController.js";
 
 export const devRouter = express.Router();
 
 devRouter.route("/projects").get(sendProjects);
+devRouter.route("/blogs").get(sendBlogs)
